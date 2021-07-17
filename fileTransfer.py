37261f7a44f852becb9238759eae2efd1d0cb752
shobha-tmx <85308084+shobha-tmx@users.noreply.github.com>
2021-07-17 00:05:25 -0400

    Add files via upload

--- fileTransfer.py
+++ fileTransfer.py
@@ -5,31 +5,28 @@ from datetime import timezone
 from dateutil.tz import tzutc
 import subprocess
 
+import azcopy
 
 current_datetime = datetime.datetime.now(timezone.utc)
 time = 30
 
 SOURCE_BUCKET="tmx-sb-source"
-#CERT_PATH="https://tmx-sb-source.s3.ca-central-1.amazonaws.com/tmx-config-files/csamx_cert_out.pem"
+CERT_PATH="tmx-sb-source/tmx-config-files/csamx_cert_out.pem"
 
 def file_transfer(file):
-        s3_resource = boto3.resource('s3')
+	s3_resource = boto3.resource('s3')
 
-        copy_source = {
-                'Bucket': SOURCE_BUCKET,
-                'Key': file
-        }
-        source1=copy_source
-        CERT_PATH = {
-            'Bucket': SOURCE_BUCKET,
-            'Key': "tmx-config-files/csamx_cert_out.pem"
-        }
-        #s3_resource.meta.client.copy(copy_source, DESTINATION_BUCKET, file)
-        subprocess.call(["azcopy", "login", "--service-principal", "--certificate-path={0}".format(CERT_PATH),
-                         "--application-id=420fce6f-04b5-42e2-aa24-65e948201ac3",
-                         "--tenant-id=015637ad-dbce-4b9c-a607-525b6b1cff39"])
+	copy_source = {
+	        'Bucket': SOURCE_BUCKET,
+	        'Key': file
+	}
+	#s3_resource.meta.client.copy(copy_source, DESTINATION_BUCKET, file)
+	subprocess.call(["azcopy", "login", "--service-principal", "--certificate-path={0}".format(CERT_PATH),
+                     "--application-id=420fce6f-04b5-42e2-aa24-65e948201ac3",
+                     "--tenant-id=015637ad-dbce-4b9c-a607-525b6b1cff39"])
 
-        subprocess.call(["azcopy","copy","{0}".format(source1),"https://csamxstorage.blob.core.windows.net/mxblob-uat/test1"])
+    subprocess.call(["azcopy", "copy", copy_source,
+                     "https://csamxstorage.blob.core.windows.net/mxblob-uat/test1"])
 
 s3 = boto3.client('s3')
 
@@ -38,7 +35,7 @@ objs = s3.list_objects_v2(Bucket=SOURCE_BUCKET)['Contents']
 for obj in objs:
 	if not obj["Key"].endswith("/"):
 		time_diff = (current_datetime - obj["LastModified"]).total_seconds()/60
-		if time_diff > time:
+		if time_diff < time:
 			file_transfer(obj["Key"])
 			print(obj["Key"] + " copied successfully.")
 		else:
